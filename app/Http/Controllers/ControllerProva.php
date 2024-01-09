44a7c53510c936c90183115a6512a88ffefe10d3
Andrea <andreadiblasi904@gmail.com>
2024-01-09 13:56:28 +0100

    orario finito

---
+++ app/Http/Controllers/ControllerProva.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerProva extends Controller
+{
+     /*public function time($now = now())  
+    {
+        return  $now;
+    }*/
+}
---
+++ resources/views/pagina.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>prova</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div>
+            <h1>Hello Laravel</h1>
+            <p>{{$now = now()}}</p>
+        </div>
+    </body>
+</html>
